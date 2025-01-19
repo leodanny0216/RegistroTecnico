2a2557de2247f7def6829a3e30f719545b3ee803
leodanny02 <leodannymercedes@gmail.com>
2025-01-18 21:38:28 -0400

    Cree la carpeta Dal.

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -5,7 +5,7 @@ namespace RegistroTecnicos.DAL;
 
 public class Contexto : DbContext
 {
-    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+   public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
 }

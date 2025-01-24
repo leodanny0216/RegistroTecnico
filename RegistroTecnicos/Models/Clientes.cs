67559236345d0595bbfd376ad9e29f3db538c824
leodanny02 <leodannymercedes@gmail.com>
2025-01-23 22:22:58 -0400

    Conexión a la  base de datos

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿namespace RegistroTecnicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "Llenar este campo por favor.")]
+        public string? Nombres { get; set; }
+        [Required(ErrorMessage = "Llenar este campo por favor.")]
+        public string? WhatsApp { get; set; }
+    }
+}
+}

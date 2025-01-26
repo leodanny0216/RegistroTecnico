b68c7456f13a713507cc42cc1baf5e6d63d86bc3
leodanny02 <leodannymercedes@gmail.com>
2025-01-26 00:42:27 -0400

    Cree el componete Clientes

--- RegistroTecnicos/Models/Clientes.cs
+++ RegistroTecnicos/Models/Clientes.cs
@@ -1,35 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RegistroTecnicos.Models;
-
-public class Clientes
+namespace RegistroTecnicos.Models
 {
-    internal int clientesId;
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
 
-    [Key]
-    public int ClienteId { get; set; }
+        // Fechalngreso should be DateTime if it's meant to store a date
+        public DateTime? Fechalngreso { get; set; }  // Change to DateTime if it's meant to represent a date
 
-    [Required(ErrorMessage = "Llenar este campo por favor.")]
-    public string? Nombres { get; set; }
-  
-    [Required(ErrorMessage = "Llenar este campo por favor.")]
-    public string? Direccion { get; set; }
+        public string? Nombres { get; set; }
 
-    [Required(ErrorMessage = "Llenar este campo por favor.")]
-    [MaxLength(10, ErrorMessage = "El RNC no debe exceder los 10 caracteres.")]
-    public string? Rnc { get; set; }
+        // Fixed typo: changed "Direcion" to "Direccion"
+        public string? Direccion { get; set; }
 
-    [Required(ErrorMessage = "Llenar este campo por favor.")]
-    [Range(0, double.MaxValue, ErrorMessage = "El límite de crédito debe ser un número positivo.")]
-    public decimal LimiteCredito { get; set; }
+        public string? Rnc { get; set; }
 
-    [Required(ErrorMessage = "Llenar este campo por favor.")]
-    public DateTime FechaIngreso { get; set; }
+        // Ensure LimiteCredito is decimal
+        public decimal LimiteCredito { get; set; }
 
-    [ForeignKey("TecnicoId")]
-    public int TecnicoId { get; set; }
+        // TecnicoId is likely a foreign key for a related entity
+        
 
-    public Tecnicos? TecnicosId { get; set; }
-   
+        [ForeignKey("Tecnicos")]
+        public int TecnicoId { get; set; }
+        public Tecnicos? Tecnicos { get; set; }
+
+    }
 }

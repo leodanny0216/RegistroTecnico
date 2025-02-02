7f377a6af0426b27d6dadbbe884f0ec57ab9f218
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-02-02 18:48:15 -0400

    cree en en el contexto los Tickets

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -9,4 +9,22 @@ public class Contexto : DbContext
 
    public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Tickets> Tickets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tickets>()
+            .HasOne(t => t.Tecnicos)
+            .WithMany()
+            .HasForeignKey(t => t.TecnicoId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Tickets>()
+            .HasOne(t => t.Cliente)
+            .WithMany()
+            .HasForeignKey(t => t.ClienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }

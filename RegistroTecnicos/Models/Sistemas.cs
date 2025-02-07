2697b3940ca8f20c6d1edc1189fb7008a2579fa8
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-02-07 00:23:48 -0400

    hice el modelo Sistemas

---
+++ RegistroTecnicos/Models/Sistemas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Modelsss
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string? Complejidad { get; set; }
+    }
+}
